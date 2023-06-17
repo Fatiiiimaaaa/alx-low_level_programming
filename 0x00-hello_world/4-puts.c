0cb6b65468dadcdf6f37f7c0337a2f8e4ebc2081
Fatiiiimaaaa <oumhamedfatima0@gmail.com>
2023-06-16 18:47:17 -0700

    Fix task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,12 @@
 
 /**
 
- *  puzzle, followed by a newline.
- *  @return: Always 0.
+ *  main - First main
+ *
+ *  Return: Always 0.
  */
 int main(void)
-
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
