76328f8847d01ba0323dcf5ef941c9f0a986d29b
Fatiiiimaaaa <oumhamedfatima0@gmail.com>
2023-06-19 18:31:35 -0700

    Go task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 0 to 9.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+       	int number;
+	for (number = 0; number < 10; number++)
+	{
+		putchar(number);
+    	}
+    	putchar('\n');
+    	return (0);
+}
