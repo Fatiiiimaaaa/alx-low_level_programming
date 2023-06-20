77314d4eacba15e15aaa0736dda62855384d11e2
Fatiiiimaaaa <oumhamedfatima0@gmail.com>
2023-06-19 18:29:09 -0700

    Go task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,12 +7,11 @@
  */
 int main(void)
 {
-    int number;
-
-    for (number = 0; number < 10; number++)
-    {
-        printf("%d", number);
-    }
-    putchar('\n');
-    return 0;
+       	int number;
+	for (number = 0; number < 10; number++)
+	{
+		printf("%d", number);
+    	}
+    	putchar('\n');
+    	return (0);
 }
