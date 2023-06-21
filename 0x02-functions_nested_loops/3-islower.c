9bf9764e010e7b0581798888a325aa51be5fc996
Fatiiiimaaaa <oumhamedfatima0@gmail.com>
2023-06-20 17:12:55 -0700

    tafta

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - check if the letter  is lowercase
+ * @c: is the letter to be checked
+ * Return: 1 if letter is lowercase, otherwise 0.
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
+
